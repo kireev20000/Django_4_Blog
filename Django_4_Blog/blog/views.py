8262be0d9b49452f28706759b09ed4c2be989cc4
Alexsandr Kireev <kireev20000@mail.ru>
2023-03-06 19:17:21 +0700

    added search rank by relevancy

--- Django_4_Blog/blog/views.py
+++ Django_4_Blog/blog/views.py
@@ -1,11 +1,13 @@
 """Вью-сет приложения Post."""
 from django.core.mail import send_mail
-from django.core.paginator import Paginator, EmptyPage,\
-                                  PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, \
+    SearchQuery, \
+    SearchRank
 from taggit.models import Tag
 from django.db.models import Count
 
@@ -99,6 +101,7 @@ def post_comment(request, post_id):
                                                        'form': form,
                                                        'comment': comment})
 
+
 def post_search(request):
     """Полнотекстовый поиск по PostGres ДБ."""
     form = SearchForm()
@@ -108,9 +111,13 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + \
+                SearchVector('body', weight='B')
+            search_query = SearchQuery(query, config='russian')
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+                ).filter(rank__gte=0.3).order_by('-rank')
     return render(request, 'blog/post/search.html', {'form': form,
                                                      'query': query,
                                                      'results': results})
