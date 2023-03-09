751b820018a81a9d9d06f0d0f8f33e8bcbb315fc
Alexsandr Kireev <kireev20000@mail.ru>
2023-03-09 17:17:08 +0700

    added custom auth app and header menu

--- Django_4_Blog/Django_4_Blog/settings.py
+++ Django_4_Blog/Django_4_Blog/settings.py
@@ -128,6 +128,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = 'auth:dashboard'
+LOGIN_URL = 'auth:login'
+LOGOUT_URL = 'auth:logout'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- Django_4_Blog/account/views.py
+++ Django_4_Blog/account/views.py
@@ -4,25 +4,30 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 
-def user_login(request):
-    """Функция логина пользователя."""
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Аутентификация прошла успешно!')
-                else:
-                    return HttpResponse('Ваш аккаунт неактивен!')
-            else:
-                HttpResponse('Неверный логин или пароль!')
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+# def user_login(request):
+#     """Функция логина пользователя."""
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request,
+#                                 username=cd['username'],
+#                                 password=cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Аутентификация прошла успешно!')
+#                 else:
+#                     return HttpResponse('Ваш аккаунт неактивен!')
+#             else:
+#                 HttpResponse('Неверный логин или пароль!')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'account/login.html', {'form': form})
