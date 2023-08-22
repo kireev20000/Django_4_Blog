1e9649f797b48a7a5b27d92c7b33441357a29f59
Alexsandr <kireev20000@mail.ru>
2023-08-22 19:06:24 +0700

    override form.save() in ImageCreateForm

--- Django_4_Blog/images/forms.py
+++ Django_4_Blog/images/forms.py
@@ -1,6 +1,10 @@
 """Формы для картинок."""
 
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
+import requests
 
 from .models import Image
 
@@ -16,11 +20,28 @@ class ImageCreateForm(forms.Form):
             'url': forms.HiddenInput,
         }
 
+    def save(self, force_insert=False,
+                   force_update=False,
+                   commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data('url')
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        response = requests.get(image_url)
+        image.image.save(image_name,
+                         ContentFile(response.content),
+                         save=False)
+        if commit:
+            image.save()
+
+        return image
+
     def clean_url(self):
-        """Проверка что картинка имеет нужный формат."""
+        """Проверка, что картинка имеет нужный формат."""
         url = self.cleaned_data['url']
         valid_extensions = ['jpeg', 'jpg', 'png']
-        extension = url.rsplit(',', 1)[1].lower()
+        extension = url.rsplit('.', 1)[1].lower()
         if extension not in valid_extensions:
             raise forms.ValidationError('Формат картинки может быть'
                                         ' только jpeg, jpg или png.')
